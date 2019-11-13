2ea9c6e22ddafcae6627574ab6ab3b03e04d7b00
Andy Shen <andyshen55@ucla.edu>
2019-11-13 00:02:10 -0800

    added modularized rand program

---
+++ Lab6/randlibhw.c
@@ -0,0 +1,17 @@
+#include "randlib.h"
+#include <immintrin.h>
+
+/* Return a random value, using hardware operations.  */
+static unsigned long long
+hardware_rand64 (void)
+{
+  unsigned long long int x;
+  while (! _rdrand64_step (&x))
+    continue;
+  return x;
+}
+
+extern unsigned long long rand64 (void)
+{
+  return hardware_rand64();
+}
