1e46e41c99151fa62eff1335652c773f33707770
Andy Shen <andyshen55@ucla.edu>
2019-12-22 14:52:15 -0800

    did not complete hw, just checks for git repository

--- Lab9/topo_order_commits.py
+++ Lab9/topo_order_commits.py
@@ -1,18 +1,50 @@
 #!/usr/local/cs/bin/python3
 
-from os import listdir, getcwd
-from os.path import dirname
+from os import listdir, getcwd, chdir
+from os.path import dirname, join, abspath
 from sys import stderr
+from zlib import decompress
 
+#sorry aaron i decided to spend my time studying instead of finishing this hw
 def topo_order():
-    if not discover(getcwd()):
-        print("Not inside a Git repository", file=stderr)
-        return 1
+    git_dir = discover(getcwd())
+    chdir(join(git_dir, ".git"))
+    commit_tree = []
     
+    branch_dir = join(getcwd(), "refs", "heads")
+    branches = [join(branch_dir, branch) for branch in listdir(branch_dir)]
+    print(branches)
+
+    for branch in branches:
+        dfs_commits(branch)
+
+            
+def dfs_commits(branch):
+    with open(branch, 'r') as f:
+        hash = f.read().strip('\n')
+        hash_dir = join(getcwd(), "objects", hash[:2], hash[2:])
+
+        with open(hash_dir, 'rb') as commit_blob:
+            decompressed = decompress(commit_blob.read()).decode()
+            for line in decompressed.splitlines():
+                if "parent" in line:
+                    parents = line.split()[1:]
+                    
 def discover(path):
     if path == "/":
-        return False
-    return ".git" in listdir(path) or discover(dirname(path))
+        print("Not inside a Git repository", file=stderr)
+        exit(1)
+
+    if ".git" in listdir(path):
+        return path
+    
+    return discover(dirname(path))
+
+class CommitNode:
+    def __init__(self, commit_hash):
+        self.commit_hash = commit_hash
+        self.parents = set()
+        self.children = set()
     
 if __name__ == '__main__':
     topo_order()
